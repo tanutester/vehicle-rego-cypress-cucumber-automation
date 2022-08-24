9f2afe24167285c2ffccbd98f213f69a4a786b5d
tanutester <tanupriya775@gmail.com>
2022-08-24 22:40:22 +1000

    created cypress config files

---
+++ cypress/e2e/rego.cy.js
@@ -0,0 +1,5 @@
+describe('Victoria rego check', () => {
+  it('open service victoria website', () => {
+    cy.visit('https://service.vic.gov.au/find-services/transport-and-driving/registration/check-registration')
+  })
+})
