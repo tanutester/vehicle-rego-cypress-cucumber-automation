051eede0468fb3528bf54d6eb5fcb16b7cc42025
tanutester <tanupriya775@gmail.com>
2022-09-05 17:44:22 +1000

    added scenarios for error validation

--- cypress/e2e/rego.feature
+++ cypress/e2e/rego.feature
@@ -4,3 +4,12 @@ Scenario: Rego Check
   Given I open service victoria page
   Then I should see landing banner
   Then I click on Get started button
+
+
+Scenario: check registration error
+  Given check registration page is opened
+  Then I should see the title
+  Then I click on continue button
+  Then I should see error box
+
+
--- cypress/support/step_definitions/rego/steps.js
+++ cypress/support/step_definitions/rego/steps.js
@@ -7,8 +7,7 @@ Given('I open service victoria page', () => {
 Then('I should see landing banner', () => {
     cy.get(".landing-banner")
         .find("h1")
-        .contains("Check registration")
-        .should('have.length', 1);
+        .should('have.text', 'Check registration');
 })
 
 Then('I click on Get started button', () => {
@@ -16,4 +15,28 @@ Then('I click on Get started button', () => {
         .find("a")
         .contains("Get started")
         .click()
+})
+
+Given('check registration page is opened', () => {
+    cy.visit("https://service.vic.gov.au/find-services/transport-and-driving/registration/check-registration/vehicle")
+})
+
+Then('I should see the title', () => {
+    cy.get("header.form-banner")
+        .find("h1")
+        .should('have.text', 'Check registration')
+})
+
+Then('I click on continue button', () => {
+    cy.get('.button-holder-wrapper')
+    .find('button')
+    .contains('Continue')
+    .click()
+
+})
+
+Then("I should see error box", () => {
+    cy.get('.form-summary.is-error')
+    .find('p')
+    .should('have.text','The following field contains an error:')
 })
