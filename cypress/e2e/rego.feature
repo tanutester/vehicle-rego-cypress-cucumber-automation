f2732be7cd51ec5a3d3dd45e04b0eba77ed3d012
tanutester <tanupriya775@gmail.com>
2022-09-06 12:39:51 +1000

    added test scenarios for registration status

--- cypress/e2e/rego.feature
+++ cypress/e2e/rego.feature
@@ -1,15 +1,48 @@
-Feature: Service Victoria
+Feature: check the rego successfully for service victoria.
+
+"""Scenario: select vehicle type
+  Given I am on registration detail page
+  Then I should see the title
+  When I click on vehicle type "car"
+  When I click on vehicle type "motorcycle"
+  When I click on vehicle type "caravan" """
+
 
 Scenario: Rego Check
-  Given I open service victoria page
+  Given I am on the registration page
   Then I should see landing banner
-  Then I click on Get started button
+  When I click on Get started button
+  Then I should see the vehicle detail page
 
 
 Scenario: check registration error
-  Given check registration page is opened
+  Given I am on registration detail page
   Then I should see the title
-  Then I click on continue button
+  When I click on continue button
   Then I should see error box
 
 
+Scenario: check wrong registration number
+  Given I am on registration detail page 
+  Then I should see the title
+  When I enter rego number "gthb"
+  And I click on continue button
+  Then I should get the error message
+
+
+Scenario: check car registration success
+  Given I am on registration detail page
+  Then I should see the title
+  When I enter rego for vehicle type "car" "1VR9HR"
+  And I click on continue button
+  Then I should see the status "Vehicle registered"
+
+
+Scenario: check motorcycle registration success
+  Given I am on registration detail page
+  Then I should see the title
+  When I click on motorcycle
+  And I enter rego for vehicle type "motorcycle" "1Y7VX"
+  And I click on continue button
+  Then I should see the status "Registration cancelled"
+  
--- cypress/support/step_definitions/rego/steps.js
+++ cypress/support/step_definitions/rego/steps.js
@@ -1,6 +1,6 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given('I open service victoria page', () => {
+Given('I am on the registration page', () => {
     cy.visit('https://service.vic.gov.au/find-services/transport-and-driving/registration/check-registration')
 })
 
@@ -10,14 +10,20 @@ Then('I should see landing banner', () => {
         .should('have.text', 'Check registration');
 })
 
-Then('I click on Get started button', () => {
+When('I click on Get started button', () => {
     cy.get(".transaction-actions")
         .find("a")
         .contains("Get started")
         .click()
 })
 
-Given('check registration page is opened', () => {
+Then('I should see the vehicle detail page', () => {
+    cy.get("header.form-banner")
+        .find("h1")
+        .should('have.text', 'Check registration')
+})
+
+Given('I am on registration detail page', () => {
     cy.visit("https://service.vic.gov.au/find-services/transport-and-driving/registration/check-registration/vehicle")
 })
 
@@ -27,12 +33,11 @@ Then('I should see the title', () => {
         .should('have.text', 'Check registration')
 })
 
-Then('I click on continue button', () => {
+When('I click on continue button', () => {
     cy.get('.button-holder-wrapper')
     .find('button')
     .contains('Continue')
     .click()
-
 })
 
 Then("I should see error box", () => {
@@ -40,3 +45,65 @@ Then("I should see error box", () => {
     .find('p')
     .should('have.text','The following field contains an error:')
 })
+
+When("I enter rego number {string}", (rego) => {
+    cy.get('#rego-number').type(rego)
+})
+
+
+Then("I should see the status {string}",(regostatus) => {
+    cy.get('.transaction-header')
+    .find('h2')
+    .should('have.text',regostatus)
+})
+
+
+Then("I should get the error message",() => {
+    cy.get('.form-summary.is-error',{timeout: 15000})
+        .should('be.visible')
+    cy.get('.form-summary-title')
+        .find('h2')
+        .should('contain',"That number doesn't look right.")
+
+})
+
+When ("I click on motorcycle",() => {
+    cy.get('.square').find('li:nth-child(2)').click()
+
+})
+
+When("I enter rego for vehicle type {string} {string}", (vehicletype, rego) => {
+
+    let selectorX = "";
+
+    if(vehicletype === "car")
+        selectorX = "#rego-number"
+
+    if(vehicletype === "motorcycle")
+        selectorX = "#rego-number-motorcycle"
+
+    if(vehicletype === "caravan")
+        selectorX = "#rego-number-caravan"
+    
+    cy.get(selectorX).type(rego)
+
+})
+
+
+When("I click on vehicle type {string}", (vehicletype) => {
+
+    let vehicleSelector = "";
+
+    if (vehicletype === "car")
+     vehicleSelector = ".is-car"
+
+     if (vehicletype === "motorcycle")
+     vehicleSelector = ".is-motorbike"
+
+     if (vehicletype === "caravan")
+     vehicleSelector = ".is-caravan"
+
+    
+    cy.get(vehicleSelector).parent().click()
+
+}) 
