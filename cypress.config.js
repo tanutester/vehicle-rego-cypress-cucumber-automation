fdbfe31be4adc0405675951b8996a452fb9b0d3d
tanutester <tanupriya775@gmail.com>
2022-09-07 21:20:22 +1000

    Added allure reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     specPattern:  "**/*.{feature,features,spec.js,cy.js}",
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
+      allureWriter(on, config);
       // implement node event listeners here
     },
   },
